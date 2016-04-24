a0164699fa8343bf78c43555621962918c9eedb2
Edwin <e.malisevskis@gmail.com>
2016-04-24 14:39:02 +0300

    cheto

---
+++ src/main/java/hello/Person.java
@@ -0,0 +1,61 @@
+package hello;
+
+/**
+ * Created by Edwin on 2016.04.19..
+ */
+public class Person {
+
+    private String name;
+    private String surname;
+    private String password;
+    private String email;
+    private int age;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + " surname: " + surname +  "email: " + email + " password: " + password + " age: " + age;
+    }
+
+
+}
